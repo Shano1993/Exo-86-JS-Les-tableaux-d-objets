54992dbdf533e771713d4f0d376fa3677850486b
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-30 12:53:11 +0100

    voiture

--- app.js
+++ app.js
@@ -31,8 +31,23 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen, peugeot, ford], // Vos voitures dans cette propriété !
 }
+
+let para = document.getElementById('para');
+let span = document.createElement('span');
+para.prepend(span);
+
+for (let i = 0; i < voitures.collection.length; i ++) {
+    voitures.collection[i].ouvrirPorte();
+}
+
+span.innerHTML = "La citroen est de couleur" + " " + voitures.collection[0].color + "<br/>";
+span.innerHTML += "La peugeot est de couleur" + " " + voitures.collection[1].color + "<br/>";
+span.innerHTML += "La ford est de couleur" + " " + voitures.collection[2].color;
+
+
+
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
